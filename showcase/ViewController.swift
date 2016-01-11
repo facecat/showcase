769a877074c836ba98d29eda47ec4654abaf994c
facecat <facecat10002gmail.com>
2016-01-11 23:14:22 +0800

    Initial Commit

---
+++ showcase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  showcase
+//
+//  Created by 邱国邦 on 16/1/11.
+//  Copyright © 2016年 FaceCat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
